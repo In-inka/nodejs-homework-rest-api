056795e6172f3bc9257e870c9d3f6b98871321cc
Inna Shkilna <shkinai.1995@gmail.com>
2023-09-17 22:36:18 +0300

    update

--- config.js
+++
@@ -1,8 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://Inka:kapitoshka2019@cluster0.hymswcm.mongodb.net/db-contacts?retryWrites=true&w=majority";
-const PORT = 3001;
-
-module.exports = {
-  DB_HOST,
-  PORT,
-};
